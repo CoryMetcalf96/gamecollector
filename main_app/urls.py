d740b61e99fd418ffef09de1ba5caf1fe5e66761
Cory Metcalf <corymetcalf96@gmail.com>
2022-07-10 12:24:17 -0400

    Added base templates and index functionality off of seed data.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('games/index/', views.games_index, name='game_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,9 +4,24 @@ from django.http import HttpResponse
 
 # HOME PAGE
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'home.html')
 
 
 # ABOUT PAGE
 def about(request):
-    return HttpResponse('About Page')
+    return render(request, 'about.html')
+
+class Game:
+    def __init__(self, title, rating, description):
+        self.title = title
+        self.rating = rating
+        self.description = description
+
+games = [
+    Game('Fallout 3', 'PG-13', 'Post-Apocalyptic RPG / Strategy Game'),
+    Game('The Sims 4', 'E for Everyone', 'Simpulation Strategy Game.')
+]
+
+# GAMES INDEX / VIEW ALL GAMES
+def games_index(request):
+    return render(request, 'games/index.html', { 'games' : games })
